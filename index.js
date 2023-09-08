90ed7787d77b2f552363bb3f0b163db4417fda08
Nelson <111023854+Nelson501@users.noreply.github.com>
2023-09-08 16:23:43 +0100

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 const dayOfWeek = document.querySelector('[data-testid = "currentdayOfTheweek"]')
 const currentDateUtc = document.querySelector('[data-testid = "currentUtcDate"]')
 
-function updateDateTime(){
+function updateWeek(){
      dayOfWeek.textContent = new Date().toLocaleDateString('en-US', {weekday:"long"});
     //  console.log(dayOfWeek);
-     currentDateUtc.textContent= new Date().toDateString()
-    //  console.log(currentDateUtc);
-
 }
 
-setInterval(updateDateTime, 1000)
+function updateTimeUtc(){
+    currentDateUtc .textContent = new Date().toISOString().slice(11, 19) + "UTC";
+      
+}
+
+setInterval(updateWeek, 1000);
+updateTimeUtc();
